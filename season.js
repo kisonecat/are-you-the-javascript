5f537a35dbb80b49db173688b2b72c788478662e
Jim Fowler <fowler@math.osu.edu>
2016-06-21 21:40:17 -0400

    Episode two

--- season.js
+++ season.js
@@ -1,175 +1,72 @@
-var men = ['Asaf',
-	   'Cam',
-	   'Cameron',
-	   'Giovanni',
-	   'John',
-	   'Morgan',
-	   'Prosper',
-	   'Sam',
-	   'Stephen',
-	   'Tyler'];
+var men = [
+  "Asaf",
+  "Cam",
+  "Cameron",
+  "Giovanni",
+  "John",
+  "Morgan",
+  "Prosper",
+  "Sam",
+  "Stephen",
+  "Tyler"
+];
 
-var women = ['Alyssa',
-	     'Camille',
-	     'Emma',
-	     'Francesca',
-	     'Julia',
-	     'Kaylen',
-	     'Mikala',
-	     'Nicole',
-	     'Tori',
-	     'Victoria'];
-
-var truthBooths =
-	[
-	    { 'Prosper': {'Tori': false } },
-	    /*
-	{ 'Devin': {'Kiki': false } },
-	{ 'Zak':   {'Kiki': false } },
-	{ 'Chuck': {'Britni': false } },
-	{ 'Connor': {'Chelsey': true } },
-	{ 'Chuck': {'Kiki': false } },
-	{ 'Alec': {'Melanie': false } },
-	{ 'Nelson': {'Kiki': false } },
-	{ 'Hunter': {'Britni': false } },
-	{ 'Zak': {'Kayla': true } },
-	     */
-    ];
+var women = [
+  "Alyssa",
+  "Camille",
+  "Emma",
+  "Francesca",
+  "Julia",
+  "Kaylen",
+  "Mikala",
+  "Nicole",
+  "Tori",
+  "Victoria"
+];
 
 var matchUps = [
-    [3, {
-	'Alyssa': 'Sam',
-	'Camille': 'Prosper',
-	'Emma': 'John',
-	'Francesca': 'Asaf',
-	'Julia': 'Morgan',
-	'Kaylen': 'Giovanni',
-	'Mikala': 'Cameron',
-	'Nicole': 'Stephen',
-	'Tori': 'Tyler',
-	'Victoria': 'Cam'}
-    ],
-    /*
-    [2, {
-	'Hannah': 'Chuck',
-	'Rashida': 'Tyler',
-	'Amanda': 'Mike',
-	'Melanie': 'Devin',
-	'Chelsey': 'Connor',
-	'Kiki': 'Austin',
-	'Stacey': 'Alec',
-	'Britni': 'Hunter',
-	'Cheyenne': 'Nelson',
-	'Kayla': 'Zak'
-    }],
-    [0, {
-	'Hunter': 'Stacey',
-	'Austin': 'Kiki',
-	'Connor': 'Kayla',
-	'Nelson': 'Cheyenne',
-	'Mike': 'Amanda',
-	'Zak': 'Britni',
-	'Devin': 'Melanie',
-	'Tyler': 'Rashida',
-	'Alec': 'Chelsey',
-	'Chuck': 'Hannah'
-    }],
-    [3, {
-	'Chelsey': 'Connor',
-	'Kiki': 'Chuck',
-	'Amanda': 'Austin',
-	'Rashida': 'Devin',
-	'Cheyenne': 'Tyler',
-	'Melanie': 'Nelson',
-	'Hannah': 'Zak',
-	'Kayla': 'Mike',
-	'Stacey': 'Alec',
-	'Britni': 'Hunter'
-    }],
-    [2,{
-	'Alec': 'Amanda',
-	'Tyler': 'Melanie',
-	'Zak': 'Cheyenne',
-	'Chuck': 'Kiki',
-	'Devin': 'Hannah',
-	'Mike': 'Kayla',
-	'Connor': 'Chelsey',
-	'Nelson': 'Britni',
-	'Hunter': 'Rashida',
-	'Austin': 'Stacey'
-    }],
-    [1+1,{
-	'Melanie': 'Hunter',
-	'Britni': 'Mike',
-	'Stacey': 'Alec',
-	'Kayla': 'Zak',
-	'Rashida': 'Nelson',
-	'Cheyenne': 'Devin',
-	'Hannah': 'Austin',
-	'Kiki': 'Chuck',
-	'Amanda': 'Tyler',
-	'Chelsey': 'Connor' // From earlier Truth Booth
-    }],
-    [3,{
-	'Alec': 'Stacey',
-	'Austin': 'Cheyenne',
-	'Chuck': 'Amanda',
-	'Connor': 'Chelsey',
-	'Devin': 'Rashida',
-	'Hunter': 'Britni',
-	'Mike': 'Melanie',
-	'Nelson': 'Kiki',
-	'Tyler': 'Hannah',
-	'Zak': 'Kayla'
-    }],
-    [3,{
-	'Alec': 'Rashida',
-	'Austin': 'Kayla',
-	'Chuck': 'Melanie',
-	'Connor': 'Chelsey',
-	'Devin': 'Britni',
-	'Hunter': 'Amanda',
-	'Mike': 'Stacey',
-	'Nelson': 'Kiki',
-	'Tyler': 'Cheyenne',
-	'Zak': 'Hannah'
-    }],
-    [3,{
-	'Alec': 'Stacey',
-	'Austin': 'Kiki',
-	'Chuck': 'Amanda',
-	'Connor': 'Chelsey',
-	'Devin': 'Rashida',
-	'Hunter': 'Britni',
-	'Mike': 'Melanie',
-	'Nelson': 'Kayla',
-	'Tyler': 'Cheyenne',
-	'Zak': 'Hannah'
-    }],
-    [2,{
-	'Alec': 'Stacey',
-	'Austin': 'Cheyenne',
-	'Chuck': 'Rashida',
-	'Connor': 'Chelsey',
-	'Devin': 'Britni',
-	'Hunter': 'Kayla',
-	'Mike': 'Kiki',
-	'Nelson': 'Amanda',
-	'Tyler': 'Melanie',
-	'Zak': 'Hannah'
-    }],
-    [10,{
-	'Alec': 'Amanda',
-	'Austin': 'Britni',
-	'Chuck': 'Melanie',
-	'Connor': 'Chelsey',
-	'Devin': 'Rashida',
-	'Hunter': 'Hannah',
-	'Mike': 'Kiki',
-	'Nelson': 'Stacey',
-	'Tyler': 'Cheyenne',
-	'Zak': 'Kayla'
-    }],                    
-     */
+  [
+    3,
+    {
+      "Asaf": "Francesca",
+      "Cam": "Victoria",
+      "Cameron": "Mikala",
+      "Giovanni": "Kaylen",
+      "John": "Emma",
+      "Morgan": "Julia",
+      "Prosper": "Camille",
+      "Sam": "Alyssa",
+      "Stephen": "Nicole",
+      "Tyler": "Tori"
+    }
+  ],
+  [
+    3,
+    {
+      "Asaf": "Camille",
+      "Cam": "Julia",
+      "Cameron": "Mikala",
+      "Giovanni": "Kaylen",
+      "John": "Nicole",
+      "Morgan": "Alyssa",
+      "Prosper": "Emma",
+      "Sam": "Francesca",
+      "Stephen": "Tori",
+      "Tyler": "Victoria"
+    }
+  ]
+];
+
+var truthBooths = [
+  {
+    "Prosper": {
+      "Tori": false
+    }
+  },
+  {
+    "John": {
+      "Julia": false
+    }
+  }
 ];
 
